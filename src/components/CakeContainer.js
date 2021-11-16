4ecfdcfec94c9f5a55916b6aba2e98fe979feee8
VinodKumar Muppera <vinod.muppera@vedantu.com>
2021-11-16 11:19:14 +0530

    basic biolerplate

---
+++ src/components/CakeContainer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const CakeContainer = () => {
+    return(
+        <div>
+            <h2>Number of Cakes</h2>
+            <button>Buy a cake</button>
+        </div>
+    )
+}
+
+export default CakeContainer;
